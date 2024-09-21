2b1a43256c8834cfbb2b57000e73c7481df40626
francispinheiro <efransys@gmail.com>
2024-09-21 15:50:43 -0300

    First steps in java with Spring Boot 3

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/efransys/RestWithSpringBootAndJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.efransys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaErudioApplication.class, args);
+	}
+
+}
